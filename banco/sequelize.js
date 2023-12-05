ffb9c3d9b96ebd62988f96b5da16835ba89f8128
jefferson-justino <115052745+jefferson-justino@users.noreply.github.com>
2023-12-05 18:30:43 -0300

    ff

--- banco/sequelize.js
+++ banco/sequelize.js
@@ -1,6 +1,14 @@
 const Sequelize = require('sequelize')
+require('dotenv').config();
 // const databaseConfig = require('./database')
 // const sequelize = new Sequelize(databaseConfig)
+const sequelize = new Sequelize(process.env.DATABASE_URL,{ 
+    dialectOptions: {
+        ssl: {
+          require: true, // indica que o SSL é necessário
+          rejectUnauthorized: false // permite a conexão mesmo se não for possível verificar o certificado SSL
+        }
+      } 
+}) 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
-module.exports =sequelize 
+module.exports =sequelize  
